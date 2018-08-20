3b77290de6187d900c6ce756f60ae4fae9c18252
Sergey Matyukevich <geomatsi@gmail.com>
2018-08-20 23:06:45 +0300

    panic: minimal app
    Attempt to write minimal application:
    
    - LED2 blink
    - no-op panic implementation
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

---
+++ src/bin/panic.rs
@@ -0,0 +1,68 @@
+#![feature(panic_implementation)]
+#![no_main]
+#![no_std]
+
+#[macro_use(entry, exception)]
+extern crate cortex_m_rt as rt;
+extern crate cortex_m as cm;
+
+extern crate stm32f0;
+
+use core::panic::PanicInfo;
+use stm32f0::stm32f0x0;
+
+#[panic_implementation]
+fn panic(_panic: &PanicInfo) -> ! {
+    loop {
+        cm::asm::bkpt();
+    }
+}
+
+entry!(main);
+
+fn main() -> ! {
+    let peripherals = stm32f0x0::Peripherals::take().unwrap();
+
+    led2_init(&peripherals);
+
+    loop {
+        led2_blink(&peripherals, 3000, 1000);
+    }
+}
+
+fn led2_init(p: &stm32f0x0::Peripherals) {
+    let gpioa = &p.GPIOA;
+    let rcc = &p.RCC;
+
+    // enable GPIOA peripheral clock
+    rcc.ahbenr.write(|w| w.iopaen().set_bit());
+
+    // configure PA5 as output pin
+    gpioa.moder.write(|w| w.moder5().output());
+
+    // configure PA5 pin as pull-down
+    gpioa.pupdr.write(|w| w.pupdr5().pull_down());
+}
+
+fn led2_blink(p: &stm32f0x0::Peripherals, t1: u32, t2: u32) {
+    let gpioa = &p.GPIOA;
+
+    loop {
+        gpioa.odr.write(|w| w.odr5().set_bit());
+        delay(t1);
+        gpioa.odr.write(|w| w.odr5().clear_bit());
+        delay(t2);
+    }
+}
+
+fn delay(count: u32) {
+    for _ in 0..count {
+        cm::asm::nop();
+    }
+}
+
+exception!(*, default_handler);
+
+fn default_handler(irqn: i16) {
+    panic!("Unhandled exception (IRQn = {})", irqn);
+}
